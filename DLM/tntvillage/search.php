81485711d68f0c89aeefcbc48b693280d70e6dcd
lumo10 <luciomorelli10@gmail.com>
2018-07-18 23:55:29 +0200

    code splitted | preparing for multipage searches

--- DLM/tntvillage/search.php
+++ DLM/tntvillage/search.php
@@ -4,42 +4,37 @@ class DLMSearchTntVillage
 {
 
 
-    private $wurl = 'http://www.tntvillage.scambioetico.org/src/releaselist.php';
+    private $debug = false;
+    private $url = 'http://www.tntvillage.scambioetico.org/src/releaselist.php';
+    private $query = '';
+    private $currentPage = 1;
 
 
-    private $qurl = '';
-
-    public function __construct()
+    public function __construct($debug = false)
     {
+        $this->debug = $debug;
+        if($this->debug){
+            //header("Content-Type: text/plain");
+            $response = file_get_contents('./response.html');
+            //echo $response;
+            echo "<pre>";
+            var_dump($this->get_rows($response));
+        }
 
-        $this->qurl = $this->wurl . $this->qurl;
     }
 
     public function prepare($curl, $query)
     {
-
-        $url = $this->qurl;
-
-        $fields = array(
-            'cat' => '0',
-            'page' => '1',
-            'srcrel' => urlencode($query)
-        );
-
-        //url-ify the data for the POST
-        $fields_string = '';
-        foreach ($fields as $key => $value) {
-            $fields_string .= $key . '=' . $value . '&';
-        }
-        rtrim($fields_string, '&');
+        $this->query = $query;
+        $fields_string = $this->fields_string($this->query);
 
         //set the url, number of POST vars, POST data
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_POST, count($fields));
+        curl_setopt($curl, CURLOPT_URL, $this->url);
+        curl_setopt($curl, CURLOPT_POST, 3);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $fields_string);
 
         curl_setopt($curl, CURLOPT_FAILONERROR, 1);
-        curl_setopt($curl, CURLOPT_REFERER, $url);
+        curl_setopt($curl, CURLOPT_REFERER, $this->url);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, 20);
@@ -48,10 +43,7 @@ class DLMSearchTntVillage
 
     public function parse($plugin, $response)
     {
-        $pattern = '/<tr>(.+?)<\/tr>/ms';
-        preg_match_all($pattern, $response, $rows);
-
-        $rows = array_slice($rows[1], 1);
+        $rows = $this->get_rows($response);
 
         foreach ($rows as $key => $value){
 
@@ -86,11 +78,38 @@ class DLMSearchTntVillage
 
         $res = count($rows);
 
-        /*for ($i = 0; $i < count($links[1]); $i++) {
-            $linkType = $i % 4;
-            $link = $links[1][$i];
+        return $res;
+    }
 
-            if ($linkType == 1) {
+    private function fields_string($query){
+        $fields = array(
+            'cat' => '0',
+            'page' => $this->currentPage,
+            'srcrel' => urlencode($query)
+        );
+
+        //url-ify the data for the POST
+        $fields_string = '';
+        foreach ($fields as $key => $value) {
+            $fields_string .= $key . '=' . $value . '&';
+        }
+        rtrim($fields_string, '&');
+
+        return $fields_string;
+    }
+
+    public function get_rows($response){
+
+        $rows = $this->rows($response);
+
+        $pattern = '/<span class=\'total\' a=\'(.*?)\'>/m';
+        preg_match_all($pattern, $response, $result);
+        $totalPage = $result[1][0];
+
+//        for ($currentPage = 1; $currentPage <= $totalPage; $currentPage++) {
+//            if($currentPage == 1)
+//                continue;
+//
 //                $curlDown = curl_init($link);
 //                curl_setopt_array($curlDown, array(
 //                    CURLOPT_RETURNTRANSFER => 1,
@@ -99,62 +118,26 @@ class DLMSearchTntVillage
 //                ));
 //                $downPage = curl_exec($curlDown);
 //                curl_close($curlDown);
-                //$plugin->addResult($linkType, $downPage, $linkType, $linkType, $link, $linkType, $linkType, $linkType, $linkType);
-                //$res++;
-
-                $title = "Unknown title";
-                $download = "Unknown download";
-                $size = 0;
-                $datetime = "1970-01-01";
-                $page = "Default page";
-                $hash = "Hash unknown";
-                $seeds = 0;
-                $leechs = 0;
-                $category = "Unknown category";
-
-//                $titlePattern = "/width='8' height='8' \/>&nbsp;<b>(.+)<\/b>(.+)<\/td>/";
-//                preg_match_all($titlePattern, $downPage, $titleArray);
-//                $title = $titleArray[1][0] . $titleArray[2][0];
-
-//                $magnetPattern = "/(magnet:.+?)'/";
-//                preg_match_all($magnetPattern, $downPage, $magnet);
-//                $download = $magnet[1][0];
-                $download = $link;
-
-//                $sizePattern = "/Dimensione:<\/td>\s*<td>\s*(\d+)\s*mb/";
-//                preg_match_all($sizePattern, $downPage, $sizeArray);
-//                $size = $sizeArray[1][0] * 1024 * 1024;
-
-//                $datePattern = "/id='last_seed'>\\s*(\\d{4}-\\d{2}-\\d{2}) \\d{2}:\\d{2}:\\d{2}/";
-//                preg_match_all($datePattern, $downPage, $dateArray);
-//                $datetime = $dateArray[1][0];
-
-//                $page = $link;
 //
-//                $hashPattern = "/Info_hash:<\\/td><td>\\s*(.+?)\\s*<\\/td>/";
-//                preg_match_all($hashPattern, $downPage, $hashArray);
-//                $hash = $hashArray[1][0];
-
-//                $seedsPattern = "/id='seeders'>\\s*(\\d*)\\s*<\\/td>/";
-//                preg_match_all($seedsPattern, $downPage, $seedsArray);
-//                $seeds = $seedsArray[1][0];
+//            echo $currentPage;
+//        }
 //
-//                $leechsPattern = "/id='leechers'>\\s*(\\d*)\\s*<\\/td>/";
-//                preg_match_all($leechsPattern, $downPage, $leechsArray);
-//                $leechs = $leechsArray[1][0];
+//
+//        var_dump($totalPage);
+//        die;
 
-                $plugin->addResult($title, $download, $size, $datetime, $page, $hash, $seeds, $leechs, $category);
-                $res++;
-            }
 
-        }*/
-        return $res;
+
+        return $rows;
     }
+
+    private function rows($response){
+        $pattern = '/<tr>(.+?)<\/tr>/ms';
+        preg_match_all($pattern, $response, $rows);
+        $rows = array_slice($rows[1], 1);
+        return $rows;
+    }
+
 }
 
-//header("Content-Type: text/plain");
-//
-//$response = file_get_contents('./response.html');
-////echo $response;
-//$class = new DLMSearchTntVillage();
-//$class->parse(null, $response);
+//new DLMSearchTntVillage(true);
